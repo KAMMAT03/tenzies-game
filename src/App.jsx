d30c47c55af13c54428163c7453c4c1bea267d69
Kamil Matuszewski <kmatuszewski2310@gmail.com>
2023-10-09 23:48:18 +0200

    random dices and button implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import Dice from "./components/Dice"
+import React from "react";
 
 export default function App() {
+  const [dice, setDice] = React.useState(allNewDices());
 
-  
+  function allNewDices() {
+    return Array(10).fill(null).map(() => Math.ceil(Math.random() * 6));
+  }
+
+  const diceElements = dice.map((val, index) => <Dice value={val} key={index} />);
 
   return (
     <main>
       <div className="container">
-        <Dice value="1" />
-        <Dice value="2" />
-        <Dice value="3" />
-        <Dice value="4" />
-        <Dice value="5" />
-        <Dice value="3" />
-        <Dice value="2" />
-        <Dice value="6" />
-        <Dice value="4" />
-        <Dice value="1" />
+        {diceElements}
       </div>
+      <button>Roll</button>
     </main>
   )
 }
