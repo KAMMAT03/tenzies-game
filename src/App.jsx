afdc2a773e253b136d87dca2ee001121e305e6f2
Kamil Matuszewski <kmatuszewski2310@gmail.com>
2023-10-09 23:12:04 +0200

    dices generated and styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import Dice from "./components/Dice"
 
 export default function App() {
+
+  
+
   return (
     <main>
-      
+      <div className="container">
+        <Dice value="1" />
+        <Dice value="2" />
+        <Dice value="3" />
+        <Dice value="4" />
+        <Dice value="5" />
+        <Dice value="3" />
+        <Dice value="2" />
+        <Dice value="6" />
+        <Dice value="4" />
+        <Dice value="1" />
+      </div>
     </main>
   )
 }
---
+++ src/components/Dice.jsx
@@ -0,0 +1,8 @@
+
+export default function Dice(prop){
+    return (
+        <div className="dice">
+            {prop.value}
+        </div>
+    )
+}
